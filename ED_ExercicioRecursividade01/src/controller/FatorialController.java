30424f3de49475e4f71e01ce1e7c1290ec7e3163
Hideki <Hideki@192.168.0.15>
2021-08-18 00:48:38 -0300

    Segundo teste

--- ED_ExercicioRecursividade01/src/controller/FatorialController.java
+++ ED_ExercicioRecursividade01/src/controller/FatorialController.java
@@ -8,7 +8,7 @@ public class FatorialController {
 
 	public int fatorial(int n) {
 
-		// Condição de parada
+		// Condição de parada n == 1
 		// Enquanto o n for > que 1, o cálculo será executado 
 		// O resultado ficará armazenado na função fatorial e depois será decrementado em 1
 		// Quando chegar a n = 1, retornará o valor 1 
