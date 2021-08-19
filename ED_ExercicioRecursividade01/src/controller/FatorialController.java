d062467d00a90a339b925bb78b8cfa6bc077291f
Hideki <Hideki@192.168.0.15>
2021-08-19 00:25:55 -0300

    Considerações finais

--- ED_ExercicioRecursividade01/src/controller/FatorialController.java
+++ ED_ExercicioRecursividade01/src/controller/FatorialController.java
@@ -10,12 +10,11 @@ public class FatorialController {
 
 		// Condição de parada n == 1
 		// Enquanto o n for > que 1, o cálculo será executado 
-		// O resultado ficará armazenado na função fatorial e depois será decrementado em 1
-		// Quando chegar a n = 1, retornará o valor 1 
-
 		if (n == 1) {
+			// Quando chegar a n = 1, retornará o valor 1 
 			return 1;
 		} else {
+			// O resultado ficará armazenado na função fatorial e depois será decrementado em 1
 			return n * fatorial(n - 1);
 		}
 
--- ED_ExercicioRecursividade01/src/view/Principal.java
+++ ED_ExercicioRecursividade01/src/view/Principal.java
@@ -1,5 +1,6 @@
 package view;
 
+// Biblioteca que mostra a janela com a solicitação ao usuário
 import javax.swing.JOptionPane;
 
 import controller.FatorialController;
@@ -8,15 +9,20 @@ public class Principal {
 
 	public static void main(String[] args) {
 
+		// Método construtor
 		FatorialController fat = new FatorialController();
 
+		// Entrada de dados
 		int n = Integer.parseInt(JOptionPane.showInputDialog("Digite um número entre 1 e 12 "
 				+ "para saber o seu fatorial"));
 
+		// Condição para aceite da informação digitada, se estiver dentro do intervalo 
 		if (n > 0 && n <= 12) {
 			int fatorial = fat.fatorial(n);
+			// Resultado final
 			System.out.println("O fatorial de "+n + "! é " + fatorial);
 		} else {
+			// Mensagem caso a informação digitada não esteja de acordo com a estrutura condicional
 			System.out.print("Número inválido, digite outro número dentro do intervalo");
 		}
 		
